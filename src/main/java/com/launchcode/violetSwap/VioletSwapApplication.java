1408fa966bac426ed7486599735d4896ae9cc59b
Ashley Hainline <aetindle@gmail.com>
2024-01-16 13:14:39 -0600

    add initial spring boot project files and dependencies

---
+++ src/main/java/com/launchcode/violetSwap/VioletSwapApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.violetSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioletSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VioletSwapApplication.class, args);
+	}
+
+}
