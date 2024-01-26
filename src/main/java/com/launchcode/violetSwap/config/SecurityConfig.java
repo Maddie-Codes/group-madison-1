d9ce3bcfa2b1989c1324df8c0707c52c716224db
Ashley Hainline <aetindle@gmail.com>
2024-01-26 15:34:27 -0600

    add path to Security config to allow users not signed in to get to registration form and fix typo in build.gradle

--- src/main/java/com/launchcode/violetSwap/config/SecurityConfig.java
+++ src/main/java/com/launchcode/violetSwap/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers(("/")).permitAll();
+                    auth.requestMatchers("/", "/register").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .oauth2Login(Customizer.withDefaults())
