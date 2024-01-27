9083193ecc89b5211f86e1e2087f398e8bb3f35b
GoldiL <goldialbina@gmail.com>
2024-01-27 08:14:40 -0600

    updating the search html page

--- src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
+++ src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
@@ -2,9 +2,9 @@ package com.launchcode.violetSwap.controllers;
 
 import com.launchcode.violetSwap.models.ListedData;
 import com.launchcode.violetSwap.models.Listing;
-import com.launchcode.violetSwap.models.data.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -13,24 +13,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("search")
+@RequestMapping("/listings/search")
 public class SearchController {
 
     @Autowired
     private ListedData listedData;
 
-    @RequestMapping
+    @GetMapping
     public String search(Model model) {
-
-      return "search";
+        return "browse/search";
     }
 
-    @PostMapping("/search")
-    public String searchListings(@RequestParam(name = "variety") String variety, Model model) {
+    @PostMapping
+    public String searchListings(@RequestParam(name = "variety") String variety,  Model model) {
         List<Listing> searchResults = listedData.findByVariety(variety);
         model.addAttribute("listings", searchResults);
         model.addAttribute("searchVariety", variety);
-        return "searchVariety";
+        return "browse/search";
     }
 
 }
--- src/main/java/com/launchcode/violetSwap/models/ListedData.java
+++ src/main/java/com/launchcode/violetSwap/models/ListedData.java
@@ -2,8 +2,12 @@ package com.launchcode.violetSwap.models;
 
 import com.launchcode.violetSwap.models.data.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+
+@Component
 public class ListedData {
 
     @Autowired
