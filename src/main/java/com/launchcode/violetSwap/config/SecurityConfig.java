17a07456ab2224836f5af8b24b1d697eaa8a1c0b
Ashley Hainline <aetindle@gmail.com>
2024-01-29 13:58:31 -0600

    add csrf token to registration form

--- src/main/java/com/launchcode/violetSwap/config/SecurityConfig.java
+++ src/main/java/com/launchcode/violetSwap/config/SecurityConfig.java
@@ -7,25 +7,24 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).csrf((csrf) -> csrf.disable());
-        return http.build();
-//        return http
-//                .authorizeHttpRequests( auth -> {
-//                    auth.requestMatchers("/", "/register").permitAll();
-//                    auth.requestMatchers("/error").permitAll();
-////                    auth.requestMatchers("/favicon.ico").permitAll();
-//                    auth.requestMatchers(HttpMethod.POST, "/register").permitAll();
-////                    auth.anyRequest().authenticated();
-//                })
-//                .oauth2Login(Customizer.withDefaults())
-//                .formLogin(Customizer.withDefaults())
-//                .build();
+
+        return http.authorizeHttpRequests( auth -> {
+                    auth.requestMatchers(HttpMethod.GET, "/", "/register", "/error").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
