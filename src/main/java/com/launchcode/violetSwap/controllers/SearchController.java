eb2cc2d36783a94db6a90c9a07bf745084f1e97c
GoldiL <goldialbina@gmail.com>
2024-02-05 19:14:37 -0600

    updating the SearchController to search the variety in the browser

--- src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
+++ src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.launchcode.violetSwap.models.data.VarietyRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,9 +29,9 @@ public class SearchController {
         return "/search/varieties";
     }
 
-    @GetMapping("/listings/{id}") //_____________________________________________Show listings in selected variety
-    public String showListingsForVariety(@RequestParam String varietySearch, Model model) {
-        Variety selectedVariety = varietyRepository.findByName(varietySearch);
+    @GetMapping("/search/variety/{id}")
+    public String showListingsForVariety(@RequestParam(required = false) String varietySearch, @PathVariable Integer id, Model model) {
+        Variety selectedVariety = varietyRepository.findById(id).orElse(null);
         if (selectedVariety != null) {
             model.addAttribute("listings", selectedVariety.getListings());
             model.addAttribute("selectedVariety", selectedVariety);
@@ -39,7 +40,25 @@ public class SearchController {
             return "redirect:/search/varieties";
         }
     }
-    //___________________________________________________________________________
+
+    //___________________________________________________________________________browse variety
+
+    @GetMapping("/browse")
+    public String browseVarieties(Model model, @RequestParam(required = false) String varietySearch) {
+        List<Variety> varieties;
+        if (varietySearch != null && !varietySearch.isEmpty()) {
+            Variety foundVariety = varietyRepository.findByName(varietySearch);
+            if (foundVariety != null) {
+                return "redirect:/search/search/variety/" + foundVariety.getId();
+            }
+            // Handle case when variety is not found
+            return "redirect:/search";
+        } else {
+            varieties = varietyRepository.findAll();
+        }
+        model.addAttribute("varieties", varieties);
+        return "search/varieties";
+    }
 
 
 }
