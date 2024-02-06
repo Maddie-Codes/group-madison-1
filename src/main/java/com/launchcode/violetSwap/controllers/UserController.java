735ae59efd29c2a50f7abad8b3338b4f5a86ff9f
Ashley Hainline <aetindle@gmail.com>
2024-02-06 14:20:56 -0600

    get username from oauth login

--- src/main/java/com/launchcode/violetSwap/controllers/UserController.java
+++ src/main/java/com/launchcode/violetSwap/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,11 +28,18 @@ public class UserController {
     public String displayUserPage(HttpServletRequest request, Model model) {
 
         Principal principal = request.getUserPrincipal();
+        String authUsername;
 
-        User currentUser = userRepository.findByUsername(principal.getName());
+        if (principal instanceof OAuth2AuthenticationToken) {
+            authUsername = ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("login");
+        } else {
+            authUsername = principal.getName();
+        }
+
+        User currentUser = userRepository.findByUsername(authUsername);
 
         if (currentUser == null) {
-            User newUser = new User(principal.getName());
+            User newUser = new User(authUsername);
             userRepository.save(newUser);
             currentUser = newUser;
         }
