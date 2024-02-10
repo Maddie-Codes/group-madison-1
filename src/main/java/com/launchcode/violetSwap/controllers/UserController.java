3cc45a22aa9dfe497794e7da055be97cf876fcec
Ashley Hainline <aetindle@gmail.com>
2024-02-10 13:54:35 -0600

    Update to make gmail login work with username creation at login

--- src/main/java/com/launchcode/violetSwap/controllers/UserController.java
+++ src/main/java/com/launchcode/violetSwap/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.launchcode.violetSwap.controllers;
 import com.launchcode.violetSwap.models.User;
 import com.launchcode.violetSwap.models.data.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,14 +24,42 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/myDetails")
-    public String displayUserPage(HttpServletRequest request, Model model) {
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping
+    public String setUser(HttpServletRequest request) {
 
         Principal principal = request.getUserPrincipal();
         String authUsername;
 
         if (principal instanceof OAuth2AuthenticationToken) {
+            // github
             authUsername = ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("login");
+
+            if (authUsername == null) {
+                // gmail
+                String tokenEmail = ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("email");
+                authUsername = tokenEmail.split("@")[0];
+            }
         } else {
             authUsername = principal.getName();
         }
@@ -42,6 +71,16 @@ public class UserController {
             userRepository.save(newUser);
             currentUser = newUser;
         }
+
+        setUserInSession(request.getSession(), currentUser);
+
+        return "redirect:/user/myDetails";
+    }
+
+    @GetMapping("/myDetails")
+    public String displayUserPage(HttpServletRequest request, Model model) {
+
+        User currentUser = getUserFromSession(request.getSession());
 
         model.addAttribute("user", currentUser);
 
