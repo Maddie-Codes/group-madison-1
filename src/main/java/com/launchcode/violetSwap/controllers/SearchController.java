178e91fc124cf43f3cc6e06b7fc2a729b9ca3e4c
Anna <anrussell67@gmail.com>
2024-02-12 22:46:46 -0600

    some things fixed, some still broken

--- src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
+++ src/main/java/com/launchcode/violetSwap/controllers/SearchController.java
@@ -20,18 +20,15 @@ public class SearchController {
     @Autowired
     private VarietyRepository varietyRepository;
 
-    @Autowired
     private Search search; //instance of search, so we can call search methods
 
 
 
 
-    @GetMapping("/varieties")//_________________________________________________Browse All Varieties
-    public String browseVarieties(Model model) {
+    @GetMapping("/varieties")//_________________________________________________Browse All Varieties and can search
+    public String searchVarieties(Model model) {
         List<Variety> varieties = varietyRepository.findAll();
-        String varietySearch = null;
         model.addAttribute("varieties", varieties);
-        model.addAttribute("varietySearch", varietySearch);
         return "/search/varieties";
     }
 
@@ -51,7 +48,7 @@ public class SearchController {
 
 
     @PostMapping("/varieties")//_____________________________________________________________search for a variety
-    public String searchVarieties(Model model, String varietySearch) {
+    public String processSearchVarieties(Model model, @RequestParam String varietySearch) {
         List<Variety> varieties; //field for varieties
 
         if (varietySearch != null && !varietySearch.isEmpty()) { //if varietySearch is present
@@ -59,19 +56,19 @@ public class SearchController {
             List<Variety> varietyList = search.searchVarieties(varietySearch); //search for variety, returns a list of varieties that contain the search term(s)
 
             if (varietyList != null) { //if varietyList is present
+
                 if (varietyList.size() == 1){//if there's only 1 variety in varietyList, redirect to search/variety w/ the id.
                     Variety singleVariety = varietyList.get(0);
                     return "redirect:/search/variety/" + singleVariety.getId();
+
                 }else{ //if there are multiple varieties in varietyList, add them to model, and return "search/varieties"
                     model.addAttribute("varieties",varietyList);
                     return "search/varieties";
                 }
             }
-
-            // if variety is not found, return to browse-varieties
+            // if varietylist is not found, return to browse-varieties
             return "redirect:/search/varieties";
         }
-
         //if varietySearch is null or empty, return to search/browse-varieties
         return "redirect:/search/varieties";
     }
